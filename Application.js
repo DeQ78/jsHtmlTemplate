7568805d991778501a1aad81cfd536750bbb6a21
DeQ78 <DeQ78@github>
2024-04-11 00:16:16 +0200

    html parser

--- Application.js
+++ Application.js
@@ -1,91 +1,106 @@
+"use strict";
 export default class Application {
-    componentId = 0;
+    this_ = null;
 
-    parentHtmlEl = null;
-    parentComponent = null;
-
-    config = {
-        "componentName": ''
-    };
+    conf_ = {
+        "componentName": '',
+        "componentId": 1000000000,
+        "htmlElement": null,
+        "parentComponent": null,
+        "attributes": null
+    }
 
     template = '';
-    _virTpl = document.createDocumentFragment();
+    virDom_ = document.createDocumentFragment();
+    cloneVirDom_ = null;
 
     data = {};
     components = {};
-    attributes = {};
 
-    constructor(parentHtmlEl, parentComponent) {
-        console.log('start', 'constructor')
+    constructor(conf = {"htmlElement": null, "parentComponent": null}) {
+        console.log('start', 'constructor xxxx')
         if (this.constructor == Application) {
             throw new Error("Application is abstract classes and can't be instantiated.")
         }
-        this.config.componentName = this.constructor.name
 
-        this.parentHtmlEl = parentHtmlEl
-        this.parentComponent = parentComponent
+        // this.conf_.componentId =
+        this.conf_.componentName = this.constructor.name
 
-        console.log('end', 'constructor')
+        if (conf.hasOwnProperty('htmlElement')) {
+            this.conf_.htmlElement = document.getElementById(conf.htmlElement.slice(1));
+            if (this.conf_.htmlElement == null) {
+                throw new Error(`Can't find in html element ${conf.htmlElement}`)
+            }
+        }
+        this.conf_.parentComponent = conf.hasOwnProperty('parentComponent') ? conf.parentComponent : null;
+        this.conf_.attributes = conf.hasOwnProperty('attributes') ? conf.attributes : null;
+
+        // console.log('end', 'constructor')
     }
 
     initiate() {
-        console.log('start', 'initiate');
+        // console.log('start', 'initiate');
         this.beforeCreate();
-        this.createComponent();
+        this.parseTemplate();
         this.afterCreate();
-        console.log('end', 'initiate');
+        // console.log('end', 'initiate');
     }
 
-    createComponent() {
-        console.log('start', 'createComponent');
-        console.log(this.template);
-
-        const regexpTpl = [
-            '(?=(<:))([^\\s]+)(.+?)(?=\\/>)\\/>',       // component
-            '(?={(\\$)((:)?))([^}]+)(?=})}',            // variable (static or observed value)
-            '(?={(@))([^=]+)="([^"]+)"(?=})}',          // event of html element
-            '(?=({))({)((.+?)\\((.*?)\\))(?=})}',       // call component method
-        ];
+    parseTemplate() {
+        // console.log('start', 'parseTemplate');
+        // console.log(this.template);
 
         const regexpHtmlNode = new RegExp('(<(\\/?)(:?)([^\\/>]+)(\\/?>))', 'gm');
         let match;
-
         let prevEnd = -1;
 
         let parentEl = null;
         let parentEls = [];
-        let curentEl = this._virTpl;
-        let nTxtEl;
+        let curentEl = this.virDom_;
+        let nEl;
+        let nElHtmlClassId = this.conf_.componentId;
+        let nElComponentClassId = this.conf_.componentId;
 
-
-        // nodes.push(curentEl);
         while ((match = regexpHtmlNode.exec(this.template)) !== null) {
             console.log('-----------------------------------------');
-            console.log(`prevEnd = ${prevEnd}`);
+            // console.log(`prevEnd = ${prevEnd}`);
             console.log(`Found ${match[0]} start=${match.index} end=${regexpHtmlNode.lastIndex}`);
+            console.log(match[4]);
 
             if (prevEnd > -1 && prevEnd < match.index) {
-                console.log('text before find', this.template.substring(prevEnd, match.index));
-                nTxtEl = document.createTextNode(this.template.substring(prevEnd, match.index));
-                curentEl.appendChild(nTxtEl);
+                // console.log('text before find', this.template.substring(prevEnd, match.index));
+                nEl = document.createTextNode(this.template.substring(prevEnd, match.index));
+                curentEl.appendChild(nEl);
             }
 
             prevEnd = regexpHtmlNode.lastIndex;
 
-            let nodeName = match[4].split(' ');
-            if (match[5] == '/>' && match[3] == ':') {
-                console.log('component', '\t', nodeName[0])
+            let htmlAttr = match[4].split(' ');
+
+            if (match[5] == '\/>' && match[3] == ':') {
+                // console.log('component', '\t', htmlAttr[0])
+                nEl = document.createElement('div');
+                nElComponentClassId += 1000;
+                htmlAttr[0] = `classId="id${nElComponentClassId}"`;
+                this.nodeAddAttributes_(nEl, htmlAttr)
+                nEl.className = 'component_' + htmlAttr[0];
+                curentEl.appendChild(nEl);
             } else if (match[5] == '/>') {
-                console.log('selfclose tag', '\t', nodeName[0])
+                // console.log('selfclose tag', '\t', htmlAttr[0]);
+                nEl = document.createElement(htmlAttr[0]);
+                curentEl.appendChild(nEl);
             } else if (match[5] == '>' && match[2] == '/') {
-                console.log('close tag', '\t', nodeName[0]);
+                // console.log('close tag', '\t', htmlAttr[0]);
                 curentEl = parentEls.pop();
                 parentEl = null;
             } else if (match[5] == '>' && match[2] == '') {
-                console.log('open tag', '\t', nodeName[0]);
+                // console.log('open tag', '\t', htmlAttr[0]);
                 parentEl = curentEl;
                 parentEls.push(curentEl);
-                curentEl = document.createElement(nodeName[0]);
+                curentEl = document.createElement(htmlAttr[0]);
+                nElHtmlClassId += 1;
+                htmlAttr[0] = `classId="id${nElHtmlClassId}"`;
+                this.nodeAddAttributes_(curentEl, htmlAttr);
                 parentEl.appendChild(curentEl);
             } else {
                 console.log(match);
@@ -93,33 +108,18 @@ export default class Application {
         }
 
         console.log('-----------------------------------------');
-        console.log(this._virTpl);
 
+        this.conf_.htmlElement.replaceWith(this.virDom_);
 
+        // console.log('end', 'parseTemplate')
+    }
 
-        /*
-        '<div class="appContainer">'             '<div class="appContainer">'               ""      ""      'div class="appContainer"'              ">"
-        "<div>"                                  "<div>"                                    ""      ""      "div"                                   ">"
-        '<div class="msgBox">'                   '<div class="msgBox">'                     ""      ""      'div class="msgBox"'                    ">"
-        "</div>"                                 "</div>"                                   "/"     ""      "div"                                   ">"
-        '<div class="msgBox">'                   '<div class="msgBox">'                     ""      ""      'div class="msgBox"'                    ">"
-        "</div>"                                 "</div>"                                   "/"     ""      "div"                                   ">"
-        "</div>"                                 "</div>"                                   "/"     ""      "div"                                   ">"
-        '<span class="spacer"/>'                 '<span class="spacer"/>'                   ""      ""      'span class="spacer"'                   "/>"
-        '<:baner msg="{$msg}"/>'                 '<:baner msg="{$msg}"/>'                   ""      ":"     'baner msg="{$msg}"'                    "/>"
-        "<br/>"                                  "<br/>"                                    ""      ""      "br"                                    "/>"
-        "<br />"                                 "<br />"                                   ""      ""      "br "                                   "/>"
-        "<div>"                                  "<div>"                                    ""      ""      "div"                                   ">"
-        '<button {@keydown="counterIncres()"}>'  '<button {@keydown="counterIncres()"}>'    ""      ""      'button {@keydown="counterIncres()"}'   ">"
-        "</button>"                              "</button>"                                "/"     ""      "button"                                ">"
-        '<button {@keydown="counterDecres(2)"}>' '<button {@keydown="counterDecres(2)"}>'   ""      ""      'button {@keydown="counterDecres(2)"}'  ">"
-        "</button>"                              "</button>"                                "/"     ""      "button"                                ">"
-        '<div class="box">'                      '<div class="box">'                        ""      ""      'div class="box"'                       ">"
-        "</div>"                                 "</div>"                                   "/"     ""      "div"                                   ">"
-        '<div class="box">'                      '<div class="box">'                        ""      ""      'div class="box"'                       ">"
-        "</div>"                                 "</div>"                                   "/"     ""      "div"                                   ">"
-        */
+    nodeAddAttributes_(node, attrStr) {
+        console.log(node);
+        console.log(attrStr);
+    }
 
-        console.log('end', 'createComponent')
+    getComponentClassId() {
+        return 'id' + this.conf_.componentId;
     }
 }
